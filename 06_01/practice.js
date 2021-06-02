649968215ca445c2dd3b1d9564ae5e4214dd9fa5
ZQ M <zqm@ZQs-MacBook-Pro.fios-router.home>
2021-06-01 21:45:20 -0400

    Array methods myEvery/mySome/myReduce

---
+++ 06_01/practice.js
@@ -0,0 +1,53 @@
+Array.prototype.myForEach = function(callbackfn) {
+    for (let i = 0; i < this.length; i++) {
+        callbackfn(this[i], i, this);
+    }
+}
+
+Array.prototype.myMap = function(callbackfn) {
+    const arr = [];
+    for (let i = 0; i < this.length; i++) {
+        arr.push(callbackfn(this[i], i, this));
+    }
+    return arr;
+}
+
+Array.prototype.myFilter = function(callbackfn) {
+    const arr = [];
+    for (let i = 0; i < this.length; i++) {
+
+        if (callbackfn(this[i], i, this)) {
+            arr.push(this[i])
+        }
+    }
+    return arr;
+}
+
+Array.prototype.myEvery = function(callbackfn) {
+  for (let i = 0; i < this.length; i++) {
+    if (!callbackfn(this[i], i, this)) return false;
+  }
+  return true;
+}
+
+Array.prototype.mySome = function(callbackfn) {
+  for (let i = 0; i < this.length; i++) {
+    if (callbackfn(this[i], i, this)) return true;
+  }
+  return false;
+}
+
+Array.prototype.myReduce = function(callbackfn, initialValue) {
+  let accumulator = initialValue || this[0];
+  let i = initialValue ? 0 : 1;
+  while (i < this.length) {
+    accumulator = callbackfn(accumulator, this[i], i, this);
+    i++;
+  }
+  return accumulator;
+}
+
+const arr = ['a', 'b', 'c'];
+console.log(arr.myReduce((acc, cur, i, arr) => acc + cur + cur, 'dd')); // 'ddaabbcc'
+console.log(arr.myEvery((cur, i, arr) => cur < 'd'));
+console.log(arr.mySome((cur, i, arr) => cur > 'b'));
