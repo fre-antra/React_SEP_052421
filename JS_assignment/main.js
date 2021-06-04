78f6ae41c053bb6aedb94026c6445f32da2b56ed
phogtr <phogbtr@gmail.com>
2021-06-03 17:09:58 -0700

    finished

--- JS_assignment/main.js
+++ JS_assignment/main.js
@@ -73,9 +73,10 @@ const firstUppercase = (str) => {
 const longestWord = (str) => {
   let arr = str.split(" ");
   let output = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i].length > output.length) output = arr[i];
-  }
+  // for (let i = 1; i < arr.length; i++) {
+  //   if (arr[i].length > output.length) output = arr[i];
+  // }
+  arr.forEach((e) => (e.length > output.length ? (output = e) : output));
   return output;
 };
 
@@ -224,12 +225,194 @@ const unique = (str) => {
 
 // ====================================================
 // 17
+const charOccurence = (str) => {
+  let hmap = {};
+  for (let c of str) {
+    if (c in hmap) hmap[c]++;
+    else hmap[c] = 1;
+  }
+  return hmap;
+};
+
+// console.log(charOccurence("helloworld"));
 
 // ====================================================
 // 18
+const binarySearch = (num, arr) => {
+  arr.sort((a, b) => a - b);
+  let i = 0;
+  let j = arr.length - 1;
+  while (i <= j) {
+    let mid = Math.floor((i + j) / 2);
+    if (arr[mid] > num) j = mid - 1;
+    else if (arr[mid] < num) i = mid + 1;
+    else return mid;
+  }
+
+  return -1;
+};
+
+// console.log(binarySearch(9, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]));
 
 // ====================================================
 // 19
+const largerThanNumber = (num, arr) => arr.filter((n) => n > num);
+
+// console.log(largerThanNumber(7, [1, 12, 3, 8, 10, 6, 11, 2, 13]));
 
 // ====================================================
 // 20
+const generateId = (num) => {
+  const charList = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let output = "";
+  let len = charList.length;
+  for (let i = 0; i < num; i++) {
+    output += charList[Math.floor(Math.random() * len)];
+  }
+  return output;
+};
+
+// console.log(generateId(10));
+
+// ====================================================
+// 21
+const subset = (arr, num) => {
+  let output = [[]];
+  for (let i = 0; i < arr.length; i++) {
+    let len = output.length;
+    for (let j = 0; j < len; j++) {
+      output.push(output[j].concat(arr[i]));
+    }
+  }
+  return output.filter((e) => e.length >= num);
+};
+
+// console.log(subset([1, 2, 3], 2));
+
+// ====================================================
+// 22
+const numOfOccurence = (str, ch) => {
+  let count = 0;
+  str = str.toLowerCase();
+  for (let c of str) {
+    if (ch === c) count++;
+  }
+  return count;
+};
+
+// console.log(numOfOccurence("microsoft.com", "o"));
+
+// ====================================================
+// 23
+const firstNotRepeat = (str) => {
+  let hmap = {};
+  for (let c of str) {
+    if (c in hmap) hmap[c]++;
+    else hmap[c] = 1;
+  }
+  for (let c of str) {
+    if (hmap[c] === 1) return c;
+  }
+  return "";
+};
+
+// console.log(firstNotRepeat("abacddbec"));
+
+// ====================================================
+// 24
+const bubbleSort = (arr) => {
+  let flag = false;
+  while (!flag) {
+    flag = true;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] < arr[i + 1]) {
+        let tmp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = tmp;
+        flag = false;
+      }
+    }
+  }
+  return arr;
+};
+
+// console.log(bubbleSort([12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213]));
+
+// ====================================================
+// 25
+const longestName = (arr) => {
+  let output = arr[0];
+  // for (let i of arr) {
+  //   if (i.length > output.length) output = i;
+  // }
+  arr.forEach((e) => (e.length > output.length ? (output = e) : output));
+
+  return output;
+};
+
+// console.log(longestName(["Australia", "Germany", "United States of America"]));
+
+// ====================================================
+// 26
+const longestSubstring = (str) => {
+  let hset = new Set();
+  let tmp = "";
+  let output = "";
+  for (let c of str) {
+    if (!hset.has(c)) {
+      hset.add(c);
+      tmp += c;
+    } else {
+      if (tmp.length > output.length) {
+        output = tmp;
+      }
+      hset.clear();
+      hset.add(c);
+      tmp = c;
+    }
+  }
+
+  return output;
+};
+
+// console.log(longestSubstring("abcabdcbbc"));
+
+// ====================================================
+// 27
+const longestPalindromeSubstr = (str) => {
+  let output = [];
+  let count = 0;
+  let substrs = strCombinations(str).filter((s) => isPalindrome(s) && s.length > 1);
+  substrs.forEach((s) => {
+    s.length > count ? (count = s.length) : count;
+  });
+
+  substrs.forEach((s) => {
+    s.length === count ? output.push(s) : "";
+  });
+  return output;
+};
+
+// console.log(longestPalindromeSubstr("abracadabra"));
+
+// ====================================================
+// 28
+const jsFunc = (cb, ...args) => {
+  return cb(...args);
+};
+
+// console.log(jsFunc((a, b, c) => a + b + c, 3, 7, 1));
+
+// ====================================================
+// 29
+const fnName = (fn) => {
+  return fn.name;
+};
+
+function fnName2() {
+  return arguments.callee.name;
+}
+
+// console.log(fnName(fnName));
+// console.log(fnName(isPrime));
+// console.log(fnName2());
