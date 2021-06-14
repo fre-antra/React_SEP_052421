1ea4971213ca6b5bdcbb51161782451fa8588e9d
Patrick-FRE <patrick.lin@antra.com>
2021-06-14 17:34:38 -0400

    update

---
+++ JQuery/index.js
@@ -0,0 +1,43 @@
+// // facade pattern
+
+// // let obj = $$('button.continue1');
+// // let obj2 = $$('button.continue2');
+// // console.log(obj.html === obj2.html);
+
+// // function foo(innerHtml) {
+// //   element.innerHTML = innerHtml;
+// // }
+class MyQuery {
+  constructor(selector) {
+    this.element = document.querySelector(selector);
+  }
+  html(innerHTML) {
+    this.element.innerHTML = innerHTML;
+  }
+  hide() {}
+  show() {}
+  on() {}
+}
+function $$(selector) {
+  return new MyQuery(selector);
+}
+
+// console.log('hello');
+$$('button.continue1').html('Next Step1...');
+$$('button.continue2').html('Next Step2...');
+
+// let a = $$('button.continue1').html;
+// let b = $$('button.continue2').html;
+
+var hiddenBox = $('#banner-message');
+hiddenBox.hide();
+$('#button-container button').on('click', function (event) {
+  hiddenBox.show();
+});
+
+// $$.ajax({
+//   url: 'https://jsonplaceholder.typicode.com/todos/1',
+//   success: function (result) {
+//     console.log(result);
+//   },
+// });
