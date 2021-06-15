e915d52f1825722f67255cad4775bdb04b822aad
sankeerthreddy1597 <54692080+sankeerthreddy1597@users.noreply.github.com>
2021-06-15 13:33:01 -0400

    full stack client and server init (sign up)

---
+++ full-stack-application/controllers/auth.js
@@ -0,0 +1,72 @@
+const User = require("../models/User");
+
+exports.register = async (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const user = await User.create({
+      username,
+      email,
+      password,
+    });
+
+    res.status(201).json({
+      success: true,
+      user: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({
+      success: false,
+      error: "Please enter email and password",
+    });
+  }
+
+  try {
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      res.status(404).json({
+        sucess: false,
+        error: "Invalid credentials",
+      });
+    }
+
+    const isMatch = await user.matchPasswords(password);
+
+    if (!isMatch) {
+      res.status(404).json({
+        sucess: false,
+        error: "Invalid credentials",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      token: "jwt29386934jwt",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+exports.forgotpassword = (req, res, next) => {
+  res.send("Forgot Password Route Works");
+};
+
+exports.resetpassword = (req, res, next) => {
+  res.send("Reset Password Route Works");
+};
