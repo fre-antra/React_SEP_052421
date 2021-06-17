f5e605ed515a00e4981e56595b1526c7494ed90a
phogtr <phogbtr@gmail.com>
2021-06-17 10:45:25 -0700

    jquery update

--- JQuery/index.js
+++ JQuery/index.js
@@ -7,67 +7,78 @@
 // // function foo(innerHtml) {
 // //   element.innerHTML = innerHtml;
 // // }
+
 class MyQuery {
   constructor(selector) {
-    this.element = document.querySelector(selector);
+    if (selector === document || selector === window) {
+      this.elements = [selector];
+    } else {
+      this.elements = document.querySelectorAll(selector);
+      this.elementsOrignalDisplay = [];
+      this.elements.forEach((ele) =>
+        this.elementsOrignalDisplay.push(ele.style.display)
+      );
+    }
   }
   html(innerHTML) {
-    this.element.innerHTML = innerHTML;
+    this.elements.forEach((ele) => {
+      ele.innerHTML = innerHTML;
+    });
   }
   hide() {
-    this.element.style.display = "none";
+    this.elements.forEach((ele, index) => {
+      this.elementsOrignalDisplay[index] = ele.style.display;
+      ele.style.display = 'none';
+    });
+
+    // this.elements.forEach((ele) => {
+    //   ele.style.display = ;
+    // });
+    // this.element.style.visibility = 'hidden';
   }
   show() {
-    this.element.style.display = "block";
+    this.elements.forEach((ele, index) => {
+      ele.style.display = this.elementsOrignalDisplay[index];
+    });
   }
   on(event, cb) {
-    this.element.addEventListener(event, cb);
+    this.elements.forEach((ele, index) => {
+      ele.addEventListener(event, cb);
+    });
   }
 }
-
-const ajax = (obj) => {
-  const { url, success } = obj;
-  let data;
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      data = JSON.parse(this.responseText);
-      success(data);
-    }
-  };
-  xhttp.open("GET", url, true);
-  xhttp.send();
-};
-
 function $$(selector) {
   return new MyQuery(selector);
 }
+$$(document).on('click', () => {
+  alert('test');
+});
 
 // console.log('hello');
-$$("button.continue1").html("Next Step1...");
-$$("button.continue2").html("Next Step2...");
+$$('button.continue1').html('Next Step1...');
 
 // let a = $$('button.continue1').html;
 // let b = $$('button.continue2').html;
 
-var hiddenBox = $$("#banner-message");
+var hiddenBox = $$('#banner-message');
+
 hiddenBox.hide();
-$$("#button-container button").on("click", function (event) {
+$$('#button-container button').on('click', function (event) {
   hiddenBox.show();
 });
-$$("#button-container2 button").on("click", function (event) {
-  hiddenBox.hide();
-});
 
-// $.ajax({
-//   url: "https://jsonplaceholder.typicode.com/todos/1",
-//   success: function (result) {
-//     console.log(result);
-//   },
-// });
+$$.ajax = function ({ url, success }) {
+  fetch(url)
+    .then((data) => {
+      return data.json();
+    })
+    .then((data) => {
+      success(data);
+    });
+};
 
-ajax({
-  url: "https://jsonplaceholder.typicode.com/todos/1",
+$$.ajax({
+  url: 'https://jsonplaceholder.typicode.com/todos/1',
   success: function (result) {
     console.log(result);
   },
