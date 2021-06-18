67676177fbb57dccb63c74c72f39cf04048bdcf4
phogtr <phogbtr@gmail.com>
2021-06-17 22:52:19 -0700

    webpack config

---
+++ react-starter/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  mode: "development",
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+
+  resolve: {
+    extensions: [".js"],
+  },
+
+  devtool: false,
+  devServer: {
+    contentBase: "./dist",
+  },
+};
