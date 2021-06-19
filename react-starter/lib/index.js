cb06ee42161d224e9b5cec1f1b529008276b317a
phogtr <phogbtr@gmail.com>
2021-06-18 20:25:40 -0700

    css loader style loader

--- react-starter/lib/index.js
+++ react-starter/lib/index.js
@@ -6,6 +6,8 @@ var _reactDom = _interopRequireDefault(require("react-dom"));
 
 var _HelloMessage = _interopRequireDefault(require("./components/HelloMessage/HelloMessage"));
 
+require("./index.css");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 _reactDom["default"].render( /*#__PURE__*/_react["default"].createElement(_HelloMessage["default"], {
