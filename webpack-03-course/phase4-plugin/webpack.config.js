f4e0e294d80d5cdfd8cd060f226bd4a992843811
Donghao Wu <donghao.wu.us@gmail.com>
2021-06-20 00:46:43 -0400

    formatting.

--- webpack-03-course/phase4-plugin/webpack.config.js
+++ webpack-03-course/phase4-plugin/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: __dirname + '/public/index.html',
-      filename: 'index.html'
+      filename: 'index.html',
     }),
   ],
 };
