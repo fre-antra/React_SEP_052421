98e2274da54d69b9c4c2d4fc5ca6f167a3798533
Huanliang Fan <fanhuanliang@gmail.com>
2021-06-21 16:42:38 -0700

    Walked through react component lifecyle method.

---
+++ react/Day3 react/practice/README.md
@@ -0,0 +1,10 @@
+React lifecycle
+Each component has its lifecycle.
+Four stages of its life:
+Initialization: where the component is constructed with given Props and state.
+Mounting: the component is mounted on the DOM and rendered on the webpage.
+componentDidMount(): Invoked right after rendering
+updating: When the state is updated. 
+componentDidUpdate: Invoked after component's updates and re-rendering on the DOM
+Un-mounting: Remove the component from the page.
+componentWillUnmount: Invoked before the component is removed from the page. 
---
+++ react/Day3 react/practice/src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+//ReactDOM.render(React.createElement(), document.getElementById('root'));
+
+// foo(foo2());
+
+// function foo(input) {
+//   console.log('foo');
+//   console.log(input);
+// }
+
+// function foo2() {
+//   console.log('foo2');
+// }
+
+// const arr = [1, 2, 3];
+// // declarative programing:
+// arr.forEach((item) => {
+//   console.log(item);
+// });
+// // imperative
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
