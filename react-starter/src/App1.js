ca4680ddf5ca16a676a7cd724e28fd17a13e43c6
Jianshuo Yang <jyang63@stevens.edu>
2021-06-22 18:33:38 -0400

    added some notes for life cycle methods and some hooks usages

--- react-starter/src/App1.js
+++ react-starter/src/App1.js
@@ -5,17 +5,27 @@ import React, {useState, useEffect} from "react";
  didMount, DidUpdate, WillUnmount具有相同的作用，只不过
  被合并成了一个api。
  */
+// let firstCall = true;
 const App1 = () => {
 
   const [name, setName] = useState({name: 'Dexter'})
+  const [num, setNum] = useState(0)
+  const firstCallRef = React.useRef(true)
 
   useEffect(() => {
-    console.log('Did mount')
-    console.log('Did update')
-    return () => {
-      console.log('Will unmount')
+    console.log('Did mount!')
+  }, [])
+
+  useEffect(() => {
+    if (firstCallRef.current !== true) {
+      console.log('Did Update!')
+    } else {
+      firstCallRef.current = false
     }
-  })
+    return () => {
+      console.log('Will Unmount!')
+    }
+  }, [num])
 
   const handlerClick = () => {
     setName({name: 'Sam'})
@@ -25,6 +35,7 @@ const App1 = () => {
       <>
         <h1>Hello {name.name}</h1>
         <button onClick={handlerClick}>Change Name</button>
+        <button onClick={() => setNum(num + 1)}>Click Times: {num}</button>
       </>
   )
 }
--- react-starter/src/index.js
+++ react-starter/src/index.js
@@ -4,6 +4,30 @@ import './index.css';
 import App from './App';
 import App1 from './App1'
 
+// 用useRef也能解决这个问题 两种方式
+// const xxx = useRef(0)
+// xxx.current = counter
+// 然后alert xxx.current
+let counterRef = 0
+const FcCounter = () => {
+  const [counter, setCounter] = React.useState(0)
+  counterRef = counter
+
+  const handleShowCounter = () => {
+    setTimeout(() => {
+      // 点按钮之后 +几次就会alert几
+      alert(counterRef)
+    }, 3000)
+  }
+  return (
+      <>
+        <h1>Counter: {counter}</h1>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <button onClick={handleShowCounter}>Show Counter after 3 seconds</button>
+      </>
+  )
+}
+
 ReactDOM.render(
     <React.StrictMode>
       {/*<App />*/}
