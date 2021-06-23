70705d3b170f5bfa112ecc3a4debd44696fada5c
phogtr <phogbtr@gmail.com>
2021-06-23 16:44:48 -0700

    async setState

--- react-basic/src/index.js
+++ react-basic/src/index.js
@@ -3,29 +3,55 @@ import ReactDOM from "react-dom";
 import "./index.css";
 // import App from "./App";
 
-const FnComponnet = () => {
-  const [num, setNum] = React.useState(0);
-  const firstCalRef = React.useRef(true);
-  //const [firstCall, setFirstCall] = React.useState(true); // flag
-  React.useEffect(() => {
-    console.log("didMount");
-  }, []);
+let counterRef = 0;
+const FcCounter = () => {
+  const [counter, setCounter] = React.useState(0);
+  counterRef = counter;
 
-  React.useEffect(() => {
-    if (firstCalRef.current !== true) {
-      console.log("didUpdate");
-    } else {
-      firstCalRef.current = false;
-    }
-    return () => {
-      console.log("WillUnmount");
-    };
-  }, [num]);
-
-  return <h1 onClick={() => setNum(num + 1)}>FC ,ClickTimes: {num}</h1>;
+  const handleShowCounter = () => {
+    setTimeout(() => {
+      alert(counterRef);
+    }, 3000);
+  };
+  return (
+    <>
+      <h1>Counter:{counter}</h1>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        +
+      </button>
+      <button onClick={handleShowCounter}>Show Counter After 3s</button>
+    </>
+  );
 };
 
-ReactDOM.render(<FnComponnet />, document.getElementById("root"));
+// const FnComponnet = () => {
+//   const [num, setNum] = React.useState(0);
+//   const firstCalRef = React.useRef(true);
+//   //const [firstCall, setFirstCall] = React.useState(true); // flag
+//   React.useEffect(() => {
+//     console.log("didMount");
+//   }, []);
+
+//   React.useEffect(() => {
+//     if (firstCalRef.current !== true) {
+//       console.log("didUpdate");
+//     } else {
+//       firstCalRef.current = false;
+//     }
+//     return () => {
+//       console.log("WillUnmount");
+//     };
+//   }, [num]);
+
+//   return <h1 onClick={() => setNum(num + 1)}>FC ,ClickTimes: {num}</h1>;
+// };
+
+ReactDOM.render(<FcCounter />, document.getElementById("root"));
+// ReactDOM.render(<FnComponnet />, document.getElementById("root"));
 // ReactDOM.render(<App />, document.getElementById("root"));
 //ReactDDOM.render(React.createElement(), document.getElementById('root'));
 
