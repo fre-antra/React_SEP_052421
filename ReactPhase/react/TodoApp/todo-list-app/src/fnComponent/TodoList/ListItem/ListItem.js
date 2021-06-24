141153a89512f5aa11f29d4653e92dbe7530951b
pixellancer <bchen66@asu.edu>
2021-06-24 11:33:43 -0700

    todoList-update
    update latest change

--- ReactPhase/react/TodoApp/todo-list-app/src/fnComponent/TodoList/ListItem/ListItem.js
+++ ReactPhase/react/TodoApp/todo-list-app/src/fnComponent/TodoList/ListItem/ListItem.js
@@ -1,23 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './ListItemStyle.css'
 
-export class ListItem extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     // console.log(props);
-    // }
 
-    
-    render() {
-        const { todo, removeItem } = this.props
-        return (
-            <li className='todolist__list-item'>
-                {/* <span>{this.props.todo.title}</span> */}
-                <span>{todo.title}</span>
-                <button id={todo.id} className="btn-remove" onClick={removeItem}>X</button>
-            </li>
-        )
-    }
-}
+const ListItem = ({ todo, removeItem }) => {
+    console.log(removeItem);
+    return (
+      <>
+        <li className='todolist__list-item'>
+          <span>{todo.title} </span>
+          <button id={todo.id} className="btn-remove" onClick={removeItem}>X</button>
+        </li>
+      </>
+    );
+  };
+  
 
 export default ListItem
