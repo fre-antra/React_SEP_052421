c7ae0e6ea9d679059d6a02132336a5cb6aeca407
Jianshuo Yang <jyang63@stevens.edu>
2021-06-24 17:31:55 -0400

    todo list done

--- react-starter/src/components/TodoList/TodoItem/TodoItem.js
+++ react-starter/src/components/TodoList/TodoItem/TodoItem.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './TodoItem.css';
 
 const TodoItem = props => {
-  const {todo} = props;
+  const {todo, handlerDelete} = props;
   return (
       <li className="todolist__content-item">
         <span>{todo.title}</span>
-        <button className="btn-remove">X</button>
+        <button className="btn-remove" onClick={() => handlerDelete(todo.id)}>X</button>
       </li>
   );
 }
--- react-starter/src/components/TodoList/TodoList.js
+++ react-starter/src/components/TodoList/TodoList.js
@@ -1,14 +1,49 @@
 import React from 'react';
 import './TodoList.css';
 import TodoItem from './TodoItem/TodoItem';
+import {getAllTodos, addTodo, deleteTodo} from "../../apis/Api";
+import {Todo} from "../../model/Todo";
 
 class TodoList extends React.Component {
   state = {
-    todolist: [
-      {id: 0, userId: 1, title: 'buy a book', completed: false},
-      {id: 1, userId: 2, title: 'buy a car', completed: false},
-    ],
+    inputText: '',
+    todoList: [],
   };
+
+  componentDidMount() {
+    getAllTodos().then(data => {
+      this.setState({
+        todoList: data
+      })
+    })
+  }
+
+  // onChange函数和value属性 用来双向绑定
+  handlerChange = event => {
+    this.setState({
+      inputText: event.target.value
+    })
+  }
+
+  handlerKeyUp = e => {
+    if (e.key === 'Enter') {
+      const newTodo = new Todo(1, this.state.inputText, false)
+      addTodo(newTodo).then(data => {
+        this.setState({
+          inputText: '',
+          todoList: [data, ...this.state.todoList]
+        })
+      })
+    }
+  }
+
+  handlerDelete = id => {
+    deleteTodo(id).then(data => {
+      this.setState({
+        todoList: this.state.todoList.filter(todo => todo.id !== id)
+      })
+    }).catch(e => console.log(e))
+  }
 
   render() {
     return (
@@ -20,10 +55,13 @@ class TodoList extends React.Component {
               type="text"
               className="todolist__input"
               placeholder="What are you going to do?"
+              onChange={this.handlerChange}
+              value={this.state.inputText}
+              onKeyUp={this.handlerKeyUp}
           />
           <ul className="todolist__content">
-            {this.state.todolist.map(todo => (
-                <TodoItem key={todo.id} todo={todo}/>
+            {this.state.todoList.map(todo => (
+                <TodoItem key={todo.id} todo={todo} handlerDelete={this.handlerDelete}/>
             ))}
           </ul>
         </section>
