ea2ec1c1c302e09455c6813d9284a5b5ae77a751
phogtr <phogbtr@gmail.com>
2021-06-23 21:52:19 -0700

    updated

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,7 +1,14 @@
+import React from "react";
 import "./App.css";
+import Layout from "./components/Layout/Layout";
+import TodoList from "./components/TodoList/TodoList";
 
 function App() {
-  return <>Initial</>;
+  return (
+    <Layout>
+      <TodoList></TodoList>
+    </Layout>
+  );
 }
 
 export default App;
