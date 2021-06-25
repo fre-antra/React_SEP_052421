dfd27367d0ea0e4dfa954a53b99d4ea199c72470
Yasir Hassan <hasanyasir01@gmail.com>
2021-06-25 13:41:08 -0700

    change font-style

--- react-exam-1/src/components/Article.jsx
+++ react-exam-1/src/components/Article.jsx
@@ -3,20 +3,36 @@ import styled from "styled-components";
 
 const ArticleWrapper = styled.article`
     width: 40%;
-    padding: 2rem;
+    height: 300px;
     margin: 1rem;
     border-radius: 1rem;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    /* box-shadow: ${(props) => `2px 2px 5px ${props.color}`}; */
+    font-family: "Parisienne", cursive;
+    h3 {
+        margin-bottom: 1rem;
+    }
     p {
         font-size: 1rem;
+    }
+    @media (max-width: 768px) {
+        width: 100%;
+        height: auto;
+        padding: 2rem;
+    }
+    @media (min-width: 769px) and (max-width: 991px) {
+        width: 100%;
+        padding: 2rem;
+    }
+    @media (min-width: 991px) {
+        padding: 2rem;
     }
 `;
 const Button = styled.button`
     padding: 0.5rem;
     width: 6rem;
+    margin-top: 1rem;
     background-color: ${(props) => {
         return props.type == "primary"
             ? "blue"
--- react-exam-1/src/components/Header.jsx
+++ react-exam-1/src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import styled from "styled-components";
+
+const H1 = styled.h1`
+    color: ${(props) => props.color};
+    margin: 1rem 0;
+    font-family: "Parisienne", cursive;
+`;
 
 export const Header = ({ color }) => {
     return (
         <div>
-            <h1 className="app-title" style={{ color: `${color}` }}>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            </h1>
+            <H1 color={color}>I got it; you blutooth are tired of usb</H1>
         </div>
     );
 };
