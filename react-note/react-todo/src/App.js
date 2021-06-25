4a7c59eec757358de7236ec2eb38a1c8d3be272d
phogtr <phogbtr@gmail.com>
2021-06-24 17:25:43 -0700

    moved to new folder

---
+++ react-note/react-todo/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Layout from "./components/Layout/Layout";
+import TodoList from "./components/TodoList/TodoList";
+import Dashboard from "./components/Dashboard/Dashboard";
+
+function App() {
+  return (
+    <Layout>
+      {/* <Dashboard></Dashboard> */}
+      <TodoList></TodoList>
+    </Layout>
+  );
+}
+
+export default App;
