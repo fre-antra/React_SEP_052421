971b9251bd2fe8c21e9c3d53370aeb4aa724902b
Huanliang Fan <fanhuanliang@gmail.com>
2021-06-28 14:21:01 -0700

    Replace div with article of semantic elements.

--- reactfirstexam/src/components/ContentBoxes/ContentBoxes.js
+++ reactfirstexam/src/components/ContentBoxes/ContentBoxes.js
@@ -15,7 +15,7 @@ export default function ContentBoxes({ data, handleColor }) {
   `;
 
   return (
-    <div>
+    <article>
         <Container>
           {data.map((item) => (
             <ContentBox
@@ -27,6 +27,6 @@ export default function ContentBoxes({ data, handleColor }) {
             />
           ))}
         </Container>
-    </div>
+    </article>
   );
 }
