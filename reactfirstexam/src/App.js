24e96486c9249c1bcd024c61f4a1a3c2d2af1368
Huanliang Fan <fanhuanliang@gmail.com>
2021-06-28 13:47:45 -0700

    Reset css. Using creacteGlobalStyle from styled-component for initailizing css.

--- reactfirstexam/src/App.js
+++ reactfirstexam/src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header/Header'
 import ContentBoxes from './components/ContentBoxes/ContentBoxes'
 import {data} from './DummyData'
 import React, {useState} from 'react'
+import styled, { createGlobalStyle } from "styled-components";
 
 function App() {
   const [color, setColor] = useState('')
@@ -10,8 +11,24 @@ function App() {
     setColor(colorName);
   };
 
+const GlobalStyle = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body{
+    font-size: 100%;
+    font-family: sans-serif;
+  }
+`;
+
+
   return (
     <div>
+      < GlobalStyle/>
       <Header color={color} />
       <ContentBoxes data={data} handleColor={handleColor} />
     </div>
