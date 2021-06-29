36d267d093f6194781483667e1affba2dfdafb9c
Donghao Wu <donghao.wu.us@gmail.com>
2021-06-29 16:36:02 -0400

    refactor first exam with new data structor and loading page.

--- react-first-exam/src/components/Content/Content.js
+++ react-first-exam/src/components/Content/Content.js
@@ -1,11 +1,30 @@
-import { useState } from 'react';
-import data from '../../data';
+import { useState, useEffect } from 'react';
 import ContentItem from './ContentItem/ContentItem';
 
 import './Content.css';
+import news from '../../data';
+
+function getNews() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(news);
+    }, 1000);
+  });
+}
 
 const Content = () => {
+  const [title, setTitle] = useState('');
+  const [data, setData] = useState([]);
+  const [loaded, setLoaded] = useState(false);
   const [selectedColor, setColor] = useState('');
+
+  useEffect(() => {
+    getNews().then((data) => {
+      setData(data[1]);
+      setTitle(data[0].title);
+      setLoaded(true);
+    });
+  });
 
   const handleColor = (targetColor) => {
     setColor(targetColor);
@@ -13,24 +32,34 @@ const Content = () => {
 
   return (
     <div className="main">
-      <div className="header" style={{ color: `${selectedColor}` }}>
-        <h3>
-          CDC says roughly 4,100 people have been hospitalized or died with
-          Covid breakthrough infections after vaccination
-        </h3>
-      </div>
-      <div className="content-container">
-        {data.map((el, index) => {
-          return (
-            <ContentItem
-              key={index}
-              data={el}
-              handleColor={handleColor}
-              selected={el.color === selectedColor}
-            />
-          );
-        })}
-      </div>
+      {loaded ? (
+        <>
+          <div className="header" style={{ color: `${selectedColor}` }}>
+            <h3>{title}</h3>
+          </div>
+          <div className="content-container">
+            {data.map((el, index) => {
+              return (
+                <ContentItem
+                  key={index}
+                  data={el}
+                  handleColor={handleColor}
+                  selected={el.color === selectedColor}
+                />
+              );
+            })}
+          </div>
+        </>
+      ) : (
+        <div className="loading-page">
+          <img
+            className="content-loader"
+            src="logo512.png"
+            alt="loading-loader"
+          />
+          <p>Loading...</p>
+        </div>
+      )}
     </div>
   );
 };
