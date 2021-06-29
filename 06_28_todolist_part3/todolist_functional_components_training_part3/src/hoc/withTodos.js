be3caa23313fd37734915ee2147bbc15dcd51ff3
MZQ <60275847+mzq737@users.noreply.github.com>
2021-06-29 10:39:29 -0400

    converted withTodo to functional component

--- 06_28_todolist_part3/todolist_functional_components_training_part3/src/hoc/withTodos.js
+++ 06_28_todolist_part3/todolist_functional_components_training_part3/src/hoc/withTodos.js
@@ -3,18 +3,18 @@ import { getAllTodos, deleteTodo, addTodo } from '../apis/TodoAPI';
 
 export const withTodos = (WrappedComponent) => (
   function NewComponent() {
-    const [todolist, setTodilist] = useState([]);
+    const [todolist, setTodolist] = useState([]);
 
     useEffect(() => {
       getAllTodos().then(data => {
-        setTodilist(data);
+        setTodolist(data);
       });
     }, []);
 
     const handleAddTodo = (newTodo) => {
       addTodo(newTodo).then((data) => {
         console.log('addTodo', data); // addTodo {userId: 1, title: "newtodo", completed: false, id: 201}
-        setTodilist([data, ...todolist]);
+        setTodolist([data, ...todolist]);
       });
     };
 
@@ -22,7 +22,7 @@ export const withTodos = (WrappedComponent) => (
       deleteTodo(id)
         .then((data) => {
           console.log('removeTodo', data); // removeTodo {}
-          setTodilist(todolist.filter((todo) => todo.id !== id));
+          setTodolist(todolist.filter((todo) => todo.id !== id));
         })
         .catch((err) => {
           console.warn(err);
