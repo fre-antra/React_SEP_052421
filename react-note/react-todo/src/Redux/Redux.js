8973e069bce0678d42dbefdbda008cf88a906fd5
phogtr <phogbtr@gmail.com>
2021-06-30 14:07:50 -0700

    redux

---
+++ react-note/react-todo/src/Redux/Redux.js
@@ -0,0 +1,107 @@
+// import { createStore } from "redux";
+const { createStore } = require("redux");
+
+/**
+ * This is a reducer - a function that takes a current state value and an
+ * action object describing "what happened", and returns a new state value.
+ * A reducer's function signature is: (state, action) => newState
+ *
+ * The Redux state should contain only plain JS objects, arrays, and primitives.
+ * The root state value is usually an object.  It's important that you should
+ * not mutate the state object, but return a new object if the state changes.
+ *
+ * You can use any conditional logic you want in a reducer. In this example,
+ * we use a switch statement, but it's not required.
+ */
+
+// pure function
+function counterReducer(state = { value: 0 }, action) {
+  switch (action.type) {
+    case "counter/incremented":
+      return { value: state.value + 1 };
+    case "counter/decremented":
+      return { value: state.value - 1 };
+    default:
+      return state;
+  }
+}
+
+// Create a Redux store holding the state of your app.
+// Its API is { subscribe, dispatch, getState }.
+// let store = createStore(counterReducer);
+
+export const myStore = myCreateStore(counterReducer);
+
+function myCreateStore(reducer) {
+  let listeners = [];
+  let state;
+  state = reducer(state, { type: "___INIT___" });
+
+  function getState() {
+    return state;
+  }
+
+  function subscribe(subCallback) {
+    listeners.push(subCallback);
+  }
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener());
+  }
+
+  return {
+    getState,
+    dispatch,
+    subscribe,
+  };
+}
+
+// myStore.subscribe(() => console.log(myStore.getState()));
+// myStore.subscribe(() => console.log("hello"));
+// myStore.dispatch({ type: "counter/decremented" });
+// myStore.dispatch({ type: "counter/decremented" });
+// myStore.dispatch({ type: "counter/decremented" });
+// console.log(myStore.getState());
+
+// You can use subscribe() to update the UI in response to state changes.
+// Normally you'd use a view binding library (e.g. React Redux) rather than subscribe() directly.
+// There may be additional use cases where it's helpful to subscribe as well.
+
+// store.subscribe(() => console.log(store.getState()));
+// store.subscribe(() => console.log("hello"));
+
+// // The only way to mutate the internal state is to dispatch an action.
+// // The actions can be serialized, logged or stored and later replayed.
+// store.dispatch({ type: "counter/incremented" });
+// // {value: 1}
+// store.dispatch({ type: "counter/incremented" });
+// // {value: 2}
+// store.dispatch({ type: "counter/decremented" });
+// // {value: 1}
+
+// function foo(num) {
+//   return foo2(num); // side effect => you can't control/ no idea about foo() since it relies on foo2()
+// }
+
+// // pure function: no side effect, same input same output, data is immutable
+// function foo2(num) {
+//   return num;
+// }
+
+// foo(5);
+// foo2(5);
+
+// // not pure function
+// function foo(obj) {
+//   obj.name = "patrick"; // mutable
+//   return obj;
+// }
+
+// // pure function
+// function foo2(obj) {
+//   return {
+//     ...obj, // shallow copy
+//     name: "patrick", // immutable
+//   };
+// }
--- react-note/react-todo/src/index.js
+++ react-note/react-todo/src/index.js
@@ -1,6 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { myStore } from "./Redux/Redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+class Counter extends React.Component {
+  handleAdd = () => {
+    myStore.dispatch({ type: "counter/incremented" });
+    console.log(myStore.getState());
+  };
+  handleSub = () => {
+    myStore.dispatch({ type: "counter/decremented" });
+    console.log(myStore.getState());
+  };
+
+  componentDidMount() {
+    myStore.subscribe(() => this.forceUpdate());
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Counter:{myStore.getState().value}</h1>
+        <button onClick={this.handleAdd}>Add</button>
+        <button onClick={this.handleSub}>Sub</button>
+      </>
+    );
+  }
+}
+
+// not working because of useEffect
+const useForceUpdate = () => {
+  const [update, setUpdate] = useState(0);
+  return [update, () => setUpdate(update + 1)];
+};
+
+function FCCounter() {
+  const [update, forceUpdate] = useForceUpdate();
+
+  useEffect(() => {
+    myStore.subscribe(() => forceUpdate());
+    console.log(update);
+  }, []);
+
+  const handleAdd = () => {
+    myStore.dispatch({ type: "counter/incremented" });
+    console.log(myStore.getState());
+  };
+  const handleSub = () => {
+    myStore.dispatch({ type: "counter/decremented" });
+    console.log(myStore.getState());
+  };
+
+  return (
+    <>
+      <h1>FcCounter:{myStore.getState().value}</h1>
+      <button onClick={handleAdd}>Add</button>
+      <button onClick={handleSub}>Sub</button>
+    </>
+  );
+}
+
+ReactDOM.render(<FCCounter />, document.getElementById("root"));
