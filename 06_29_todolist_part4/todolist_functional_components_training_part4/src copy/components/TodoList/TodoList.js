9926046e5c57490312c245c5314f3dfcd6f112b9
MZQ <60275847+mzq737@users.noreply.github.com>
2021-07-07 21:26:37 -0400

    training part 4 renderProps

---
+++ 06_29_todolist_part4/todolist_functional_components_training_part4/src copy/components/TodoList/TodoList.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import TodoItem from './TodoItem/TodoItem';
+import { Todo } from '../../model/Todo';
+import { withTodos } from '../../hoc/withTodos';
+import './TodoList.css';
+
+class TodoList extends React.Component {
+  state = {
+    inputText: '',
+  };
+
+  handleInputOnChange = (event) => {
+    this.setState({ inputText: event.target.value });
+  };
+
+  handleInputKeyUp = (event) => {
+    if (event.key === 'Enter') {
+      const useId = 1;
+      const title = this.state.inputText;
+      const completed = false;
+      const newTodo = new Todo(useId, title, completed);
+      const { handleAddTodo } = this.props;
+      handleAddTodo(newTodo);
+      this.setState({
+        inputText: '',
+      });
+    }
+  };
+
+  render() {
+    const { todolist, handleRemoveTodo } = this.props;
+
+    return (
+      <section className="todolist">
+        <header className="todolist__header">
+          <h4 className="heading">TodoList</h4>
+        </header>
+        <input
+          type="text"
+          className="todolist__input"
+          placeholder="What are you going to do?"
+          onChange={this.handleInputOnChange}
+          onKeyUp={this.handleInputKeyUp}
+          value={this.state.inputText}
+        />
+        <ul className="todolist__content">
+          {todolist.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              removeTodo={() => handleRemoveTodo(todo.id)}
+            ></TodoItem>
+          ))}
+        </ul>
+      </section>
+    );
+  }
+}
+
+export default withTodos(TodoList);
---
+++ 06_29_todolist_part4/todolist_functional_components_training_part4/src copy/components/WithTodosData/WithTodosData.js
@@ -0,0 +1,72 @@
+import React, { Fragment } from 'react';
+import { getAllTodos, deleteTodo, addTodo } from '../../apis/TodoAPI';
+
+class WithTodosData extends React.Component {
+  state = {
+    title: 'TodoData',
+    todolist: [],
+  };
+
+  handleAddTodo = (newTodo) => {
+    addTodo(newTodo).then((data) => {
+      this.setState({
+        todolist: [data, ...this.state.todolist],
+      });
+    });
+  };
+
+  handleRemoveTodo = (id) => {
+    deleteTodo(id)
+      .then((data) => {
+        this.setState({
+          todolist: this.state.todolist.filter((todo) => todo.id !== id),
+        });
+      })
+      .catch((err) => {
+        console.warn(err);
+      });
+  };
+
+  componentDidMount() {
+    getAllTodos().then((data) => {
+      this.setState({
+        todolist: data,
+      });
+    });
+  }
+  // as same as HOC, but render() is different
+
+  render() {
+    let header = null;
+    let content = null;
+    const { render, children, renderHeader } = this.props;
+
+    if (renderHeader) {
+      header = renderHeader(this.state.title);
+    }
+
+    if (render) {
+      content = render(
+        this.handleRemoveTodo,
+        this.HandleAddTodo,
+        this.state.todolist
+      );
+    } else if (children) {
+      content = children(
+        this.handleRemoveTodo,
+        this.HandleAddTodo,
+        this.state.todolist
+      );
+    }
+
+    return (
+      <Fragment>
+        {header}
+        {content}
+      </Fragment>
+    );
+  }
+}
+
+export default WithTodosData;
+
