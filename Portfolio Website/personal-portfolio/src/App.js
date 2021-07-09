86b1d241d9b9ab196cce3c165bd9a97576a17bb9
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-09 15:33:06 -0700

    Update the code. Make the app.js consise.

--- Portfolio Website/personal-portfolio/src/App.js
+++ Portfolio Website/personal-portfolio/src/App.js
@@ -6,43 +6,22 @@ import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import $ from 'jquery';
 
 function App() {
-
-//   const navigation = () => {
-//   $('.primary-nav').css('height', $('.logo').height());
-//   $('.primary-nav li').css(
-//     'margin-top',
-//     ($('.primary-nav').height() - $('.primary-nav li').height()) / 2 + 'px'
-//   );
-//   const initFunc = () =>{
-//     $(window).resize(function () {
-//       setTimeout(navigation, 500);
-//   });
-// }
-
-//   React.useEffect(()=> initFunc,[])
 
   return (
     <Router>
       <div className="App">
+        <Header />
         <Switch>
           <Route exact path="/">
-            <Header />
             <Home />
             <Projects />
             <Contact />
             <Footer />
           </Route>
-          <Route exact path="/portfolio">
-            <Header />
-            <Projects />
-          </Route>
-          <Route exact path="/contact">
-            <Header />
-            <Contact />
-          </Route>
+          <Route exact path="/portfolio" component={Projects}></Route>
+          <Route exact path="/contact" component={Contact}></Route>
         </Switch>
       </div>
     </Router>
