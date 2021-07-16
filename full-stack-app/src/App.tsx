6949c090ef9d8ae198291568528985bcdf161c83
Yuhui Shen <yuhui.shen@hotmail.com>
2021-07-15 22:17:17 -0400

    create my fullstack app

---
+++ full-stack-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import { Button } from "antd";
+import logo from "./logo.svg";
+import "./App.css";
+
+const App: FC = () => (
+	<div className="App">
+		<Button type="primary">Button</Button>
+	</div>
+);
+
+export default App;
